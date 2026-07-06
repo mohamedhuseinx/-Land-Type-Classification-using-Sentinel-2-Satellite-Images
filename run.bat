403bdd4a5e2de847438473cbcd849d42737a81a0
Mohamed Hussein <112055585+mohamedhuseinx@users.noreply.github.com>
2026-07-06 19:33:15 +0300

    Delete run.bat

--- run.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-title Sentinel-2 Land Classifier API
-cd /d "C:\DEPI project"
-echo Activating environment...
-call .venv\Scripts\activate.bat
-echo Starting API server...
-start "" http://localhost:8000
-uvicorn api.main:app --reload --host 0.0.0.0 --port 8000
-pause
